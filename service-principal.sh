cfacda91858f44e4d4dd99d4eb69720d02e521a2
netr0m <morten.amundsen@bouvet.no>
2023-11-30 11:53:54 +0100

    lint: ignore shellchecks

--- service-principal.sh
+++ service-principal.sh
@@ -1,4 +1,5 @@
 #! /usr/bin/env bash
+# shellcheck disable=SC2015
 set -euo pipefail
 
 SCRIPT_NAME=$(basename "$0")
@@ -40,6 +41,7 @@ function setup_colors() {
   if [[ -t 2 ]] && [[ -z "${NO_COLOR-}" ]] && [[ "${TERM-}" != "dumb" ]]; then
     NOFORMAT='\033[0m' RED='\033[0;31m' GREEN='\033[0;32m' ORANGE='\033[0;33m' BLUE='\033[0;34m' PURPLE='\033[0;35m' CYAN='\033[0;36m' YELLOW='\033[1;33m'
   else
+    # shellcheck disable=SC2034
     NOFORMAT='' RED='' GREEN='' ORANGE='' BLUE='' PURPLE='' CYAN='' YELLOW=''
   fi
 }
